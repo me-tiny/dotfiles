d17702754a8fffe24e8cf6b6e8a93766ef7dd827
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-04-21 23:09:51 +1000

    nvim: gitsigns error fix

--- fish/.config/fish/conf.d/50-tide-setup.fish
+++ fish/.config/fish/conf.d/50-tide-setup.fish
@@ -1,5 +1,18 @@
 if status is-interactive
     if not set -q tide_left_prompt_items; and type -q tide
-        tide configure --auto --style="Lean" --prompt_colors="True color" --show_time="No" --lean_prompt_height="Two lines" --prompt_connection="Disconnected" --prompt_spacing="Sparse" --icons="Few icons"
+        tide configure --auto --style='Lean' --prompt_colors='True color' --show_time='No' --lean_prompt_height='Two lines' --prompt_connection='Disconnected' --prompt_spacing='Compact' --icons='Few icons'
+        set -g __setup__clear_screen_prompt_count 0
+        function __setup__clear_screen --on-event fish_prompt
+            if test "$__setup__clear_screen_prompt_count" = 0
+                clear
+                echo "          Tide has been set up."
+                echo "          Press enter to begin."
+                set -e __setup__clear_screen_prompt_count
+            else
+                clear
+                functions -e __setup__clear_screen
+                fish_greeting
+            end
+        end
     end
 end
