1b491499052ec51a3a0e2ed4877ef7bea3fc4c6f
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-07-03 18:20:27 +1000

    hyprland: monitor scale auto-right is fixed, scaling 1.33->1.25 (mult of 1/120)

--- hypr/.config/hypr/hyprland/monitor.lua
+++ hypr/.config/hypr/hyprland/monitor.lua
@@ -2,15 +2,13 @@ hl.monitor({
     output = "DP-3",
     mode = "3840x2160@144",
     position = "0x0",
-    scale = "1.33",
+    scale = "1.25",
 })
 
 hl.monitor({
     output = "DP-4",
     mode = "2560x1440@144",
-    -- HACK: was auto-right, broken after lua conversion
-    -- relevant pr: https://github.com/hyprwm/Hyprland/pull/14393
-    position = "2880x0",
+    position = "auto-right",
     scale = "1",
 })
 
